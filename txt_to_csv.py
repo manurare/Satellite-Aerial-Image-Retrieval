adc262e4f069be5bfb9d0d61f22fd21200a914f2
manurare <mreyarea@gmail.com>
2019-06-14 11:06:03 +0200

    Minor changes

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -10,8 +10,6 @@ if __name__ == '__main__':
     command_args, _ = parser.parse_known_args()
     filename = command_args.file
 
-    filename = '400water-esquinas-LatLon-filter3.txt'
-
     with open(filename, 'r') as in_file:
         stripped = (line.strip() for line in in_file)
         lines = (line.replace(";",",").split(",") for line in stripped if line)
